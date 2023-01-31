5b9852ea4a301ee2a5117e7132f910f32a3a27bd
Angelo <angelo@osradar.com>
2023-01-31 23:13:10 +0100

    Afficher et masquer les accordéons selon les boutons

--- public/assets/js/home/tabhome.js
+++ public/assets/js/home/tabhome.js
@@ -1,6 +1,42 @@
-let first = document.querySelector('.div1');
-let second = document.querySelector('.chambres');
+let tous = document.querySelector('#all');
+let ventz = document.querySelector('#sale');
+let locass = document.querySelector('#rental');
+let meubless = document.querySelector('#furnished');
 
-first.addEventListener('mouseenter', function() {
-    second.style.display = 'none';
+let chbrs = document.querySelector('.chambres');
+let stds = document.querySelector('.studios');
+let maiz = document.querySelector('.maisons');
+let apptmts = document.querySelector('.appartements');
+let ters = document.querySelector('.terrains');
+
+tous.addEventListener("click", function() {
+  chbrs.style.display = "block";
+  stds.style.display = "block";
+  maiz.style.display = "block";
+  apptmts.style.display = "block";
+  ters.style.display = "block";
+});
+
+ventz.addEventListener("click", function() {
+  chbrs.style.display = "none";
+  stds.style.display = "block";
+  maiz.style.display = "block";
+  apptmts.style.display = "block";
+  ters.style.display = "block";
+});
+
+locass.addEventListener("click", function() {
+  chbrs.style.display = "block";
+  stds.style.display = "block";
+  maiz.style.display = "block";
+  apptmts.style.display = "block";
+  ters.style.display = "block";
+});
+
+meubless.addEventListener("click", function() {
+  chbrs.style.display = "block";
+  stds.style.display = "block";
+  maiz.style.display = "block";
+  apptmts.style.display = "block";
+  ters.style.display = "none";
 });
